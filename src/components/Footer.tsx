55c3229ecb0f531fb587afa71cb780aa04115f14
Bahadır Can <bahadircan.bc@gmail.com>
2023-12-12 01:37:30 +0300

    added footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,36 @@ import TelegramIcon from "../assets/footerIcons/telegramIcon.svg?react";
 import XIcon from "../assets/footerIcons/xIcon.svg?react";
 import GithubIcon from "../assets/footerIcons/githubIcon.svg?react";
 import LinkedinIcon from "../assets/footerIcons/linkedinIcon.svg?react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="w-full h-[30vh] flex">
-      <div className=" mt-auto w-full h-fit flex items-center justify-center gap-10 bg-accent bg-opacity-50">
-        <TelegramIcon className="h-[24px] w-[24px] fill-white" />
-        <XIcon className="h-[24px] w-[24px] fill-white" />
+    <div className="w-full h-fit text-white text-center gap-5">
+      You found a bug? <br /> Please report it to me via Telegram or Twitter.{" "}
+      <br />
+      <br /> Or you can fix it yourself and send a pull request. <br />
+      <a
+        href="https://github.com/bahadircan-bc/bahadircan.site"
+        className="inline-block"
+      >
+        <span className="underline">Go to repository.</span>
+      </a>
+      <br />
+      <br />
+      <div className="w-full h-fit flex items-center justify-center gap-10 bg-accent bg-opacity-50">
+        <Link target="_blank" to="https://t.me/bbahadircan">
+          <TelegramIcon className="h-[24px] w-[24px] fill-white" />
+        </Link>
+        <Link target="_blank" to="https://twitter.com/BahadirCaan">
+          <XIcon className="h-[24px] w-[24px] fill-white" />
+        </Link>
         <img src={exypnosLogo} alt="" className="w-[70px] h-[70px]" />
-        <GithubIcon className="h-[24px] w-[24px] fill-white" />
-        <LinkedinIcon className="h-[24px] w-[24px] fill-white" />
+        <Link target="_blank" to="https://github.com/bahadircan-bc">
+          <GithubIcon className="h-[24px] w-[24px] fill-white" />
+        </Link>
+        <Link target="_blank" to="https://www.linkedin.com/in/bahadır-can/">
+          <LinkedinIcon className="h-[24px] w-[24px] fill-white" />
+        </Link>
       </div>
     </div>
   );
